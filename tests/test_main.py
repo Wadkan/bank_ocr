8efab91ff715e6105891db9aba3bdd1c309224b7
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-04-29 17:17:59 +0200

    first tests work

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,26 @@
+import os
 from unittest import TestCase
+from os.path import dirname, abspath
+import filecmp
 
 target = __import__('main')
+parent_path = dirname(dirname(abspath(__file__)))
 
 
 class Test(TestCase):
-    def test_use_case_1_write_file_in_same_name_into_parsed_files(self):
+    def test_new_file_created(self):
         filename = "use_case_1.txt"
         target.main(filename)
-        self.assertEqual(1, 1)
+
+        output_file_with_path = os.path.join(parent_path, "parsed_files", filename)
+
+        self.assertTrue(os.path.isfile(output_file_with_path))
+
+    def test_use_case_1_create_correct_file(self):
+        filename = "use_case_1.txt"
+        target.main(filename)
+
+        output_file_with_path = os.path.join(parent_path, "parsed_files", filename)
+        correct_file_with_path = os.path.join(parent_path, "correct_parsed_files", filename)
+
+        self.assertTrue(filecmp.cmp(output_file_with_path, correct_file_with_path, shallow=False))
